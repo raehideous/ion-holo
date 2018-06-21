b6fedfac9e4ac19c18974a1bb7eb3d8e0ad4266a
Patryk <rae@MacBook-Pro-Patryk.local>
2018-06-21 22:19:37 +0200

    Initialized app. Created styling for holos. Installed websockets. To-do: fix websocket connection while app debugging.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import WebSocket from 'ws'
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,26 +12,31 @@ export class HomePage {
   @ViewChild('canvasTop') canvasLeft : ElementRef;
   @ViewChild('canvasTop') canvasRight : ElementRef;
   @ViewChild('canvasTop') canvasBottom : ElementRef;
-  private _CANVAS_TOP  : any;
-  private _CONTEXT_TOP : any;
-  private _CANVAS_LEFT  : any;
-  private _CONTEXT_LEFT : any;
-  private _CANVAS_RIGHT  : any;
-  private _CONTEXT_RIGHT : any;
-  private _CANVAS_BOTTOM  : any;
-  private _CONTEXT_BOTTOM : any;
 
   private _WS : any
 
-  ionViewDidLoad() {
-    console.log('ION did load ;)')
-    this._WS = new WebSocket('ws://demos.kaazing.com/amqp')
+  connect() {
+    console.log('Connecting...')
+    //With ionic debug server running, this crashes with CORS disabled. 
+    this._WS = new WebSocket('wss://echo.websocket.org/', {
+      origin: 'https://echo.websocket.org'
+    });
 
-    this._WS.on('message', data => {
-      console.log('Message from WS!')
-      console.log(data)
-    })
+    this._WS.on('open', () => this.handleConnectionOpened())
+    this._WS.on('message', mess => this.handleMessageReceived(mess))
+    this._WS.on('error', err => this.handleErrorReceived(err))
+
   }
 
+  handleConnectionOpened() {
+    this._WS.send('ECHOOO')  
+  }
 
+  handleMessageReceived(message) {
+    alert(message)
+  }
+
+  handleErrorReceived(err) {
+    alert(JSON.stringify(err))
+  }
 }
